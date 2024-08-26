f35dae97e9f81a207a1a2bdd247c421183a8344f
NamMinWoo91 <sksfortm21@gmail.com>
2024-08-26 17:07:00 +0900

    accounts 인증 프론트엔드 백엔드 일부 구현 blog_page model.py 일부 구현

--- blog/accounts/forms.py
+++ blog/accounts/forms.py
@@ -1,9 +1,31 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)  # 이메일 필드 추가
+
     class Meta:
         model = CustomUser
-        fields = ("username", "password1", "password2", "nickname", "profile_image")
+        fields = (
+            "username",
+            "email",
+            "password1",
+            "password2",
+            "nickname",
+            "profile_image",
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("이 이메일은 이미 사용 중입니다.")
+        return email
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    class Meta:
+        model = CustomUser
+        fields = ("username", "password")
--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,32 +1,34 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
+from django.contrib import messages
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST, request.FILES)  # 파일 업로드 처리
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("home")  # 홈으로 리디렉션
+            return redirect("/")  # 홈으로 리디렉션
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, "accounts/register.html", {"form": form})
 
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
-            return redirect("home")  # 홈으로 리디렉션
-    return render(request, "accounts/login.html")
+            return redirect("/")  # 로그인 후 리디렉션할 URL
+    else:
+        form = CustomAuthenticationForm()
+
+    return render(request, "accounts/login.html", {"form": form})
 
 
 def logout_view(request):
     logout(request)
-    return redirect("home")  # 홈으로 리디렉션
+    return redirect("/")  # 홈으로 리디렉션
